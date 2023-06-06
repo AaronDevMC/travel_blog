f878aaaa1beb212817136beb1c9b64aca9283cb6
AaronDevMC <102303574+AaronDevMC@users.noreply.github.com>
2023-06-06 14:53:09 +0200

    Update site.ts

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -2,16 +2,16 @@ import type { SiteConfig } from '$lib/types/site'
 
 export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
-  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
-  title: 'Urara',
-  subtitle: 'Sweet & Powerful SvelteKit Blog Template',
+  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'github.com/AaronDevMC/',
+  title: 'Travel blog',
+  subtitle: 'A blog used to write about highly real journeys',
   lang: 'en-US',
-  description: 'Powered by SvelteKit/Urara',
+  description: 'Expect terrible photoshops and renders here',
   author: {
-    avatar: '/assets/maskable@512.png',
-    name: 'John Doe',
+    avatar: '/assets/aaron_logo@512.png',
+    name: 'Aaron Malik',
     status: '🌸',
-    bio: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    bio: 'CEO of FTW LLC, CCO of FTW-Manufacturing'
   },
   themeColor: '#3D4451'
 }
